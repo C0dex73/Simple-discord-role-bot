90abad2a00f5f130662313e2e27421264353f48a
C0dex73 <c0d3x73@gmail.com>
2024-02-23 19:13:30 +0100

    added msgCreate event handler

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const Discord = require("discord.js");
+const { PermissionBitField } = require("discord.js");
 const bot = new Discord.Client({intents: 3276797});
 const config = require("./config.json");
+const fs = require("fs");
 
 let roleMessage = undefined;
+let pairsBuffer = config.pairs
 
 console.log("STARTING BOT...");
 
@@ -31,6 +34,44 @@ bot.on("ready", () => {
     });
 });
 
-/*bot.on("messageCreate", async (msg) => {
-    
-});*/
+bot.on("messageCreate", (msg) => {
+    if(msg.content.startsWith(config.prefix) && !msg.author.bot) {
+        let msgMember = msg.guild.members.fetch(msg.author.id);
+
+        fs.readdirSync("./commands").forEach((file) => {
+            let command = require(`./commands/${file}`);
+            if(command["name"] === msg.content.substring(1).trimStart().split(" ")[0]){
+                try{
+                    let pairsBufferUnsafe = command["run"](bot, msg, roleMessage, pairsBuffer);
+                    let toWrite = config;
+                    toWrite.pairs = pairsBufferUnsafe;
+                    fs.writeFile("./config.json", JSON.stringify(toWrite, undefined, 4), (err) => {
+                        if(err){
+                            console.error(err);
+                            msg.reply("sry, an error occured. Try again and if it persists, contact the administrators for more informations");
+                        }else{
+                            pairsBuffer = pairsBufferUnsafe;
+                        }
+                    });
+                }catch(err){
+                    msg.reply("u dumbass didnt respected the syntax");
+                    console.error(err);
+                }
+            }
+        });
+    }
+});
+
+bot.on("MessageReactionAdd", (msg, usr) => {
+    //~ roleMessage.reactions.cache
+})
+
+
+/*
+add :emoji: @role.
+==> bot add emoji on msg
+==> user click emoji (react) -> role given
+
+remove :emoji:
+==> bot remove react and all reacts
+*/
