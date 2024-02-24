d6fbdbcaece07b64f7b355e577fa17f66fed5da1
C0dex73 <c0d3x73@gmail.com>
2024-02-24 12:37:35 +0100

    add.js completed

--- commands/add.js
+++ commands/add.js
@@ -1,17 +1,21 @@
 const { PermissionFlagsBits } = require('discord.js');
+const Discord = require('discord.js');
 
 module.exports = {
     "name" : "add",
     "permissions" : PermissionFlagsBits.ManageRoles,
 
     "run": (bot, msg, roleMessage, pairsBuffer) => {
-        let emoji = msg.content.substring(1).trimStart().split('').filter(element => element != '')[0];
-        let roleID = msg.content.substring(1).trimStart().split('').filter(element => element != '')[1].slice(3, -1);
+        let emoji = msg.content.substring(1).trimStart().split(' ').filter(element => element != '')[1];
+        let roleID = msg.content.substring(1).trimStart().split(' ').filter(element => element != '')[2].slice(3, -1);
+        let emojiID = (Discord.parseEmoji(emoji).id === undefined) ? "0".concat(emoji.codePointAt(0).toString(16)) : "1".concat(Discord.parseEmoji(emoji).id.toString())
+        // emojiID  -->     1id  / 0unicode
 
-        // TODO : add pair to buffer
+        let pair = JSON.parse(`{"${emojiID}" : "${roleID}"}`);
+        pairsBuffer.push(pair);
 
-        // TODO : add react to msg (with words[1])
+        roleMessage.react(emoji);
 
-        return pairsBuffer
+        return pairsBuffer;
     }
 }
